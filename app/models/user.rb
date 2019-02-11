bb4ccc1634b81a9db4f9ff05eb2acb59d6df614d
thuyha96 <nguyenthithuyha96@gmail.com>
2019-02-11 15:34:54 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   before_save :downcase_email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :name,  presence: true, length: {maximum: Settings.max_lenght_name}
-  validates :email, presence: true, length: {maximum: Settings.max_lenght_email},
+  validates :name,  presence: true, length: {maximum: Settings.user_validates.max_lenght_name}
+  validates :email, presence: true, length: {maximum: Settings.user_validates.max_lenght_email},
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: Settings.max_lenght_password}
+  validates :password, presence: true, length: { minimum: Settings.user_validates.max_lenght_password}, allow_nil: true
 
   has_secure_password
 
